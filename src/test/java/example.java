7b85fcc6e7ef355d36920f6c936c3a21849e3791
alvintrianto <alvin.trianto97@gmail.com>
2022-11-23 09:50:59 +0700

    chenge print

--- src/test/java/example.java
+++ src/test/java/example.java
@@ -1,5 +1,5 @@
 public class example {
     public static void main(String[] args) {
-        System.out.println("Ini project maven dari dasar");
+        System.out.println("Ini project maven dari dasar tim BTECH");
     }
 }
